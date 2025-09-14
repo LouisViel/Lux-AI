d0f5d6a071a40db688d5b996bec79e4db4cfc53a
LouisViel <viel.louis.pro@gmail.com>
2025-09-14 22:51:36 +0200

    Global action buffer

--- utils.hpp
+++ utils.hpp
@@ -10,3 +10,14 @@ constexpr int MAX_TURNS = 360;
 #include "lux/kit.hpp"
 static std::reference_wrapper<kit::Agent> staticGame = std::ref(*(kit::Agent*)nullptr);
 #define ACCESS_GAME kit::Agent& gameState = staticGame.get();
+
+class MoveManager;
+static std::reference_wrapper<MoveManager> staticMoveManager = std::ref(*(MoveManager*)nullptr);
+#define ACCESS_MOVE MoveManager& moveManager = staticMoveManager.get();
+
+class MineManager;
+static std::reference_wrapper<MineManager> staticMineManager = std::ref(*(MineManager*)nullptr);
+#define ACCESS_MINE MineManager& mineManager = staticMineManager.get();
+
+static std::reference_wrapper<std::vector<std::string>> staticActions = std::ref(*(std::vector<std::string>*)nullptr);
+#define ACCESS_OUTPUT std::vector<std::string>& luxOutput = staticActions.get();
